1afdd20432f86dead444f83973817312b1104f85
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-27 15:35:09 -0300

    Service layer, component registration

---
+++ src/main/java/com/fredericoahb/wsjpa/services/UserService.java
@@ -0,0 +1,26 @@
+package com.fredericoahb.wsjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fredericoahb.wsjpa.entities.User;
+import com.fredericoahb.wsjpa.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
