d31b3ad86db92bdc20e500fd510be80644dc4300
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-27 14:45:58 -0300

    Project created

---
+++ src/main/java/com/fredericoahb/wsjpa/WsjpaApplication.java
@@ -0,0 +1,13 @@
+package com.fredericoahb.wsjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fredericoahb/wsjpa/WsjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredericoahb.wsjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
